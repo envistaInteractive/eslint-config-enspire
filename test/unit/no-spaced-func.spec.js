ae8eed3a11cf67b3d8b12c2d407a5682afe4ddda
Cristian Camilo Perez <cperez354@gmail.com>
2017-07-24 16:34:33 -0500

    no-spaced-func.spec test is finish

---
+++ test/unit/no-spaced-func.spec.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('No Spaced func', () => {
+
+    const errorMessageByEslint = 'Unexpected space between function name and paren.';
+
+    it('should raise error when Unexpected space between function name and paren', (done) => {
+
+        const textToTest = `fn
+                            ()`;
+        let result = global.executeOnText(textToTest);
+        global.expect(result).to.include(errorMessageByEslint);
+        return done();
+
+    });
+
+    it('should not raise error when Unexpected space between function name and paren', (done) => {
+
+        const textToTest = 'fn()';
+        let result = global.executeOnText(textToTest);
+        global.expect(result).to.not.include(errorMessageByEslint);
+        return done();
+
+    });
+
+});
