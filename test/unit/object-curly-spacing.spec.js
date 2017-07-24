336efe2e0c937b25a725142400a64e635b93b3c4
Cristian Camilo Perez <cperez354@gmail.com>
2017-07-24 18:38:31 -0500

    last update

--- test/unit/object-curly-spacing.spec.js
+++ test/unit/object-curly-spacing.spec.js
@@ -6,9 +6,7 @@ describe('No object curly spacing Validations', () => {
 
     it('should raise error when There is a space \'}\'.', (done) => {
 
-        const textToTest = `
-                            var obj = {\'foo\' : \'bar\'};
-                            `;
+        const textToTest = 'var obj = {\'foo\' : \'bar\'};';
         let result = global.executeOnText(textToTest);
         global.expect(result).to.include(errorMessageByEslint);
         return done();
@@ -17,9 +15,7 @@ describe('No object curly spacing Validations', () => {
 
     it('should not raise error when There is a space before \'}\'.', (done) => {
 
-        const textToTest = `
-                            var obj = { \'foo\': \'bar\' };
-                            `;
+        const textToTest = 'var obj = { \'foo\' : \'bar\' };';
         let result = global.executeOnText(textToTest);
         global.expect(result).to.not.include(errorMessageByEslint);
         return done();
