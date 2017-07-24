53c8da176da55ec57bb2753dbbe33a160e3229ba
stivenson <stivenson.rpm@gmail.com>
2017-07-24 18:59:50 -0500

    fix: solution to rule

--- test/unit/object-curly-spacing.spec.js
+++ test/unit/object-curly-spacing.spec.js
@@ -1,14 +1,12 @@
 'use strict';
 
-describe.only('No object curly spacing Validations', () => {
+describe('No object curly spacing Validations', () => {
 
     const errorMessageByEslint = 'object-curly-spacing';
 
     it('should raise error when There is a space \'}\'.', (done) => {
 
-        const textToTest = `
-                            var obj = {\'foo\' : \'bar\'};
-                            `;
+        const textToTest = 'var obj = {\'foo\' : \'bar\'};';
         let result = global.executeOnText(textToTest);
         global.expect(result).to.include(errorMessageByEslint);
         return done();
@@ -17,9 +15,7 @@ describe.only('No object curly spacing Validations', () => {
 
     it('should not raise error when There is a space before \'}\'.', (done) => {
 
-        const textToTest = `
-                            var obj = { \'foo\': \'bar\' };
-                            `;
+        const textToTest = 'var obj = { \'foo\': \'bar\' };';
         let result = global.executeOnText(textToTest);
         global.expect(result).to.not.include(errorMessageByEslint);
         return done();
