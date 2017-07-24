a66fe1b4f22b8099defb79388a10640212700aa3
Cristian Camilo Perez <cperez354@gmail.com>
2017-07-24 16:20:04 -0500

    no-multiple-empty-lines.spec test are finish

---
+++ test/unit/no-multiple-empty-lines.spec.js
@@ -0,0 +1,29 @@
+'use strict';
+
+describe('No Mixed spaces and tabs', () => {
+
+    const errorMessageByEslint = 'Too many blank lines at the beginning of file. Max of 1 allowed.';
+
+    it('should raise error when the code has 1 blank lines not allowed', (done) => {
+
+        const textToTest = `\n
+                            var foo = 5;
+                            \n\n\n
+                            var bar = 3;`;
+        let result = global.executeOnText(textToTest);
+        global.expect(result).to.include(errorMessageByEslint);
+        return done();
+
+    });
+
+    it('should not raise error when the code has 1 blank lines not allowed', (done) => {
+
+        const textToTest = `var foo = 5;
+                            var bar = 3;`;
+        let result = global.executeOnText(textToTest);
+        global.expect(result).to.not.include(errorMessageByEslint);
+        return done();
+
+    });
+
+});
