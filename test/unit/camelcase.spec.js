fcd161890024d5bd3e1e38455429e38484a2d804
Cristian Camilo Perez <cperez354@gmail.com>
2017-07-24 14:45:07 -0500

    last commit with new-parens test rule

--- test/unit/camelcase.spec.js
+++ test/unit/camelcase.spec.js
@@ -1,18 +1,23 @@
 'use strict';
 
 describe('camelcase Validations', () => {
+
     const errorMessageByEslint = 'is not in camel case';
 
     it('should raise error when a variable is not camel case', (done) => {
-        const textToTest = `sample_var = '';`;
+
+        const textToTest = 'sample_var = \'\';';
         global.expect(global.executeOnText(textToTest)).to.include(errorMessageByEslint);
         return done();
+
     });
 
     it('should not raise error when a variable is camel case', (done) => {
-        const textToTest = `sampleVar = '1';`;
+
+        const textToTest = 'sampleVar = \'1\';';
         global.expect(global.executeOnText(textToTest)).to.not.include(errorMessageByEslint);
         return done();
+
     });
 
 });
