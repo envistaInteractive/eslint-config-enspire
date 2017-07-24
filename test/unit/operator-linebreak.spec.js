282862d8788f2bc31653ac5fcfff13f6d09c3b74
stivenson <stivenson.rpm@gmail.com>
2017-07-24 14:40:29 -0500

    chore: Test for operator-linebreak rule

---
+++ test/unit/operator-linebreak.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe('operator-linebreak Validations', () => {
+    const errorMessageByEslint = 'should be placed at the end of the line';
+    it('should raise an error when linebreaks is placed before the operator', (done) => {
+        const textToTest = `var fullHeight = borderTop 
+                + innerHeight
+                + borderBottom;`;
+        global.expect(global.executeOnText(textToTest)).to.include(errorMessageByEslint);
+        return done();
+    });
+
+    it('should not raise an error when linebreaks is placed after the operator', (done) => {
+        const textToTest = `var fullHeight = borderTop +
+                innerHeight +
+                borderBottom;`;
+
+        global.expect(global.executeOnText(textToTest)).to,not.include(errorMessageByEslint);
+        return done();
+    });
+});
