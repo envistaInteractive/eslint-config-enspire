673567e44dbb51857fd322e0f6c44526488d22a6
Cristian Camilo Perez <cperez354@gmail.com>
2017-07-24 15:24:21 -0500

    solve conflict and merge

--- test/unit/padded-blocks.spec.js
+++ test/unit/padded-blocks.spec.js
@@ -1,18 +1,21 @@
 'use strict';
 
 describe('padded-blocks Validations', () => {
+
     const errorMessageByEslint = 'Block must be padded by blank lines';
     it('should raise an error by blank lines missing', (done) => {
+
         const textToTest = `if (a) {
                                 b();
 
                             }`;
-        console.log(global.executeOnText(textToTest));
         global.expect(global.executeOnText(textToTest)).to.include(errorMessageByEslint);
         return done();
+
     });
 
     it('should not raise an error by blank lines missing', (done) => {
+
         const textToTest = `if (a) {
 
                                 b(); 
@@ -20,5 +23,7 @@ describe('padded-blocks Validations', () => {
                             }`;
         global.expect(global.executeOnText(textToTest)).to.not.include(errorMessageByEslint);
         return done();
+
     });
+
 });
