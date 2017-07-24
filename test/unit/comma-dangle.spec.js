42ff60fc2d7730f1a4c16c48724cc0d49b9a5cab
Santiago Yepes <zetogk@gmail.com>
2017-07-24 16:14:29 -0500

    fix: Test for comma-dangle rule

--- test/unit/comma-dangle.spec.js
+++ test/unit/comma-dangle.spec.js
@@ -1,26 +1,32 @@
 'use strict';
 
 describe('comma-dangle Validations (option: never)', () => {
+
     const errorMessagesByEslint = {
         unexpected: 'Unexpected trailing comma',
         missing: 'Missing trailing comma'
     };
 
     it('should raise an error when a there is trailing comma', (done) => {
-        const textToTest = `var config = {\n \
-                                \tkey: 'ABC123456',\n \
-                                \tuser: 'root',\n \
+
+        const textToTest = `var config = {\n
+                                \tkey: 'ABC123456',\n
+                                \tuser: 'root',\n
                             }`;
         global.expect(global.executeOnText(textToTest)).to.include(errorMessagesByEslint.unexpected);
         return done();
+
     });
 
     it('should not raise an error when a there isn\'t trailing comma', (done) => {
-        const textToTest = `var config = {\n \
-                                \tkey: 'ABC123456',\n \
-                                \tuser: 'root'\n \
+
+        const textToTest = `var config = {\n
+                                \tkey: 'ABC123456',\n
+                                \tuser: 'root'\n
                             }`;
         global.expect(global.executeOnText(textToTest)).to.not.include(errorMessagesByEslint.unexpected);
         return done();
+
     });
+
 });
