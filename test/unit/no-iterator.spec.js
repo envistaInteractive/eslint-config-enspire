2b1ec07d760414cfe39dbf3a7f12f59dbe443a41
stivenson <stivenson.rpm@gmail.com>
2017-07-25 17:14:39 -0500

    test: Of no-iterator rule

---
+++ test/unit/no-iterator.spec.js
@@ -0,0 +1,24 @@
+'use strict';
+
+describe('no-iterator Validations', () => {
+
+    /*const errorMessageByEslint = 'xxxxx';
+    it('should raise an error because have xxxxx', (done) => {
+
+        const textToTest = 'xxx';
+        let result = global.executeOnText(textToTest);
+        global.expect(result).to.include(errorMessageByEslint);
+        return done();
+
+    });
+
+    it('should not raise an error because not have xxxxxx', (done) => {
+
+        const textToTest = 'xxx';
+        let result = global.executeOnText(textToTest);
+        global.expect(result).to.not.include(errorMessageByEslint);
+        return done();
+
+    });*/
+
+});
