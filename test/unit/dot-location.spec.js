e35e2f7ff473a5c93f528a53767388a858e8ef3b
Santiago Yepes <zetogk@gmail.com>
2017-07-25 04:00:21 -0500

    test: Test for dot-location rule

---
+++ test/unit/dot-location.spec.js
@@ -0,0 +1,28 @@
+'use strict';
+
+describe('dot-location Validations', () => {
+
+    const errorMessagesByEslint = {
+        asObject: 'Expected dot to be on same line as object',
+        asProperty: 'Expected dot to be on same line as property'
+    };
+
+    it('should raise an error when a dot is not in the same line of the property', (done) => {
+
+        const textToTest = `var name = user.\n
+                                \tname;`;
+        global.expect(global.executeOnText(textToTest)).to.include(errorMessagesByEslint.asProperty);
+        return done();
+
+    });
+
+    it('should not raise an error when a dot is in the same line of the property', (done) => {
+
+        const textToTest = `var name = user\n
+                                \t.name;`;
+        global.expect(global.executeOnText(textToTest)).to.not.include(errorMessagesByEslint.asProperty);
+        return done();
+
+    });
+
+});
