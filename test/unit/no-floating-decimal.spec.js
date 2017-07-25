310c91a7dc34359bda23c3d1a1af1a638339c7fa
stivenson <stivenson.rpm@gmail.com>
2017-07-25 12:25:02 -0500

    test: Of no-floating-decimal rule

---
+++ test/unit/no-floating-decimal.spec.js
@@ -0,0 +1,24 @@
+'use strict';
+
+describe('no-floating-decimal Validations', () => {
+
+    const errorMessageByEslint = 'A leading decimal point can be confused with a dot';
+    it('should raise an error because have Floating Decimals', (done) => {
+
+        const textToTest = 'try { var num = .5; }catch(e){}';
+        let result = global.executeOnText(textToTest);
+        global.expect(result).to.include(errorMessageByEslint);
+        return done();
+
+    });
+
+    it('should not raise an error because Floating Decimals', (done) => {
+
+        const textToTest = 'var num = 0.5;';
+        let result = global.executeOnText(textToTest);
+        global.expect(result).to.not.include(errorMessageByEslint);
+        return done();
+
+    });
+
+});
