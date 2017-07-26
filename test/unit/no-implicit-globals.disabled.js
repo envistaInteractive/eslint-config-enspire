f3aa4580404027ef832bdfed88737e63b814252b
Santiago Yepes <zetogk@gmail.com>
2017-07-26 16:32:25 -0500

    test: Test for no-implicit-globals rule

---
+++ test/unit/no-implicit-globals.disabled.js
@@ -0,0 +1,27 @@
+'use strict';
+
+describe('no-implicit-globals Validations', () => {
+
+    const errorMessageByEslint = 'Implicit global variable, assign as global property instead';
+
+    /*
+    //Is not working
+    it('should raise an error when var varName = \'value\' is used to declare a global variable', (done) => {
+
+        const textToTest = `'use strict';
+                            var myVar = 1;`;
+        global.expect(global.executeOnText(textToTest)).to.include(errorMessageByEslint);
+        return done();
+
+    });
+    */
+
+    it('should not raise an error when a global variable is set explicitly', (done) => {
+
+        const textToTest = 'window.globalVar = 1;';
+        global.expect(global.executeOnText(textToTest)).to.not.include(errorMessageByEslint);
+        return done();
+
+    });
+
+});
