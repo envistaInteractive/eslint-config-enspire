a73543486b5304aa10b4dab18e6a9e239cb7704e
stivenson <stivenson.rpm@gmail.com>
2017-07-26 00:34:37 -0500

    test: Of no-with rule

---
+++ test/unit/no-with.spec.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('no-with Validations', () => {
+
+    const errorMessageByEslint = 'Unexpected use of \'with\' statement.';
+    it('should raise an error because have xxxx', (done) => {
+
+        const textToTest = `with (point) {
+                                r = Math.sqrt(x * x + y * y); // is r a member of point?
+                            }`;
+        let result = global.executeOnText(textToTest);
+        global.expect(result).to.include(errorMessageByEslint);
+        return done();
+
+    });
+
+    it('should not raise an error because not have xxxx', (done) => {
+
+        const textToTest = 'const r = ({x, y}) => Math.sqrt(x * x + y * y);';
+        let result = global.executeOnText(textToTest);
+        global.expect(result).to.not.include(errorMessageByEslint);
+        return done();
+
+    });
+
+});
