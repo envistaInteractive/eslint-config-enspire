2eae10fea9f7cd793d5eb370b60ef46b1feafc65
stivenson <stivenson.rpm@gmail.com>
2017-07-26 00:44:58 -0500

    fix: description of test

--- test/unit/no-with.spec.js
+++ test/unit/no-with.spec.js
@@ -3,7 +3,7 @@
 describe('no-with Validations', () => {
 
     const errorMessageByEslint = 'Unexpected use of \'with\' statement.';
-    it('should raise an error because have xxxx', (done) => {
+    it('should raise an error because have \'with\'', (done) => {
 
         const textToTest = `with (point) {
                                 r = Math.sqrt(x * x + y * y); // is r a member of point?
@@ -14,7 +14,7 @@ describe('no-with Validations', () => {
 
     });
 
-    it('should not raise an error because not have xxxx', (done) => {
+    it('should not raise an error because not have \'with\'', (done) => {
 
         const textToTest = 'const r = ({x, y}) => Math.sqrt(x * x + y * y);';
         let result = global.executeOnText(textToTest);
