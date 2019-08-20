2b9123e6e81f3ddb46774146bf551177c6a60d43
dtripati <dtripati@enspirecommerce.com>
2019-08-21 02:08:46 +0530

    fix(Linting fixes): comment out not used var and format code via prettier

--- test/common.js
+++ test/common.js
@@ -6,7 +6,7 @@ const expect = chai.expect;
 const chaiString = require('chai-string');
 chai.use(chaiString);
 const common = require('../configs/common');
-const util = require('util'); // optional use
+//const util = require('util'); // optional use
 
 const engine = new CLIEngine({
     envs: ['node', 'mocha'],
@@ -21,27 +21,36 @@ const sintaxError = 'is a syntax error';
 describe('Dot-notation Validations', () => {
 
     it('should raise error when dot-notation is not used', (done) => {
+
         let text = 'b = a[\'a\'];';
         expect(executeOnText(text)).to.include(betterDotNotation);
         return done();
+
     });
     it('should not raise error when dot-notation is used', (done) => {
+
         let text = 'b = a.a;';
         expect(executeOnText(text)).to.not.include(betterDotNotation);
         return done();
+
     });
     it('should not raise error when keyword is used', (done) => {
+
         let text = 'Joi.string().default(\'defValue\');';
         expect(executeOnText(text)).to.not.include(sintaxError);
         return done();
+
     });
+
     it('should not raise error when try catch is used', (done) => {
+
         let text = 'try{}catch(e){}';
         expect(executeOnText(text)).to.not.include(sintaxError);
         return done();
-    });
-});
 
+    });
+
+});
 
 /**
 
@@ -51,8 +60,5 @@ describe('XXXXXXX Validations', () => {
  // it...
 }
 
-
 /** Example for good and big log */
 // console.log(util.inspect(executeOnText(text), true, 12, true));
-
-
